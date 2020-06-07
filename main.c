3fc309359bffa95c4009b251c429548de78c78ea
eggman <skouhei@gmail.com>
2020-06-08 06:55:53 +0900

    Add Parser.

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//
+// Tokenize
+//
+
 typedef enum {
     TK_RESERVED, // Symbol
     TK_NUM,      // Integer
@@ -130,32 +134,102 @@ Token *tokenize(char *p)
     return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // Node kind
+    Node *lhs;     // Left-hand side
+    Node *rhs;     // Right-hand side
+    int val;       // Used if kind == ND_NUM
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs  = lhs;
+    node->rhs  = rhs;
+    return node;
+}
+
+Node *new_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val  = val;
+    return node;
+}
+
+Node *expr(void)
+{
+    Node *node = new_num(expect_number());
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, new_num(expect_number()));
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, new_num(expect_number()));
+        else
+            return node;
+    }
+}
+
+void gen(Node *node)
+{
+    if (node->kind == ND_NUM) {
+        printf("  mov x0, #%d\n", node->val);
+        printf("  str x0, [sp, #-16]!\n");
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  ldr x1, [sp], #16\n");
+    printf("  ldr x0, [sp], #16\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add x0, x0, x1\n");
+        break;
+    case ND_SUB:
+        printf("  sub x0, x0, x1\n");
+        break;
+    }
+    printf("  str x0, [sp, #-16]!\n");
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2) {
         error("%s: invalid number of arguments", argv[0]);
-        return 1;
     }
 
+    // Tokenize and parse.
     user_input = argv[1];
     token      = tokenize(user_input);
+    Node *node = expr();
 
+    // Print out the first half of assembly.
     printf(".globl main\n");
     printf("main:\n");
 
-    // The first token must be a number
-    printf("  mov x0, #%d\n", expect_number());
+    // Traverse the AST to emit assembly.
+    gen(node);
 
-    // ... followed by either `+ <number>` or `- <number>`
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add x0, x0, #%d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub x0, x0, #%d\n", expect_number());
-    }
+    // A result must be at the top of the stack, so pop it
+    // to RAX to make it a program exit code.
+    printf("  ldr x0, [sp], #16\n");
 
     printf("  ret\n");
     return 0;
